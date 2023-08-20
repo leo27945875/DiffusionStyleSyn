644348a057af63dba934225f1ee641910de142a8
Leo <ezioatiar@gmail.com>
2023-08-20 23:28:32 +0800

    _

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from utils import *
 def Train(
         seed             : int        = 0,
         nEpoch           : int        = 1000,
-        batchSize        : int        = 256,
+        batchSize        : int        = 320,
         gradAccum        : int        = 8,
         lr               : float      = 2e-4,
         nWorker          : int        = 8,
